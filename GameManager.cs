7081eaa9e9305118b761635bba0802f0cbee8804
JHLee0891 <killiacaridos@gmail.com>
2020-07-18 22:39:45 +0900

    first commit

---
+++ GameManager.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] enemyObjs;
+    public Transform[] spawnPoints;
+
+    public float maxSpawnDelay;
+    public float curSpawnDelay;
+
+    public GameObject player;
+    public Text scoreText;
+    public Image[] lifeImage;
+    public GameObject gameOverSet;
+
+    void Update()
+    {
+        curSpawnDelay += Time.deltaTime;
+        if (curSpawnDelay > maxSpawnDelay)
+        {
+            SpawnEnemy();
+            maxSpawnDelay = UnityEngine.Random.Range(0.3f, 1.5f);
+            curSpawnDelay = 0;
+        }
+
+        //#.UI Score Update
+        Player playerLogic = player.GetComponent<Player>();
+        scoreText.text = string.Format("{0:n0}", playerLogic.score);
+    }
+
+    void SpawnEnemy()
+    {
+        int ranEnemy = UnityEngine.Random.Range(0, 3);
+        int ranPoint = UnityEngine.Random.Range(0, 9);
+        GameObject enemy = Instantiate(enemyObjs[ranEnemy],
+                            spawnPoints[ranPoint].position,
+                            spawnPoints[ranPoint].rotation);
+        Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
+        Enemy enemyLogic = enemy.GetComponent<Enemy>();
+        enemyLogic.player = player;
+
+        if (ranPoint == 5 || ranPoint == 6) {  //#.Right Spawn
+            enemy.transform.Rotate(Vector3.back * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed*(-1), -1);
+        }
+        else if (ranPoint == 7 || ranPoint == 8) {  //#.Left Spawn
+            enemy.transform.Rotate(Vector3.forward * 90);
+            rigid.velocity = new Vector2(enemyLogic.speed, -1);
+        }
+        else {  //#.Front Spawn
+            rigid.velocity = new Vector2(0, enemyLogic.speed*(-1));
+        }
+    }
+    public void UpdateLifeIcon(int life)
+    {
+        //#.UI Life Init Disable
+        for (int index = 0; index < 3; index++) {
+            lifeImage[index].color = new Color(1, 1, 1, 0);
+        }
+
+        //#.UI Life Active
+        for (int index=0; index < life; index++) {
+            lifeImage[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+    public void RespawnPlayer()
+    {
+        Invoke("RespawnPlayerExe", 2f);
+    }
+
+    void RespawnPlayerExe()
+    {
+        player.transform.position = Vector3.down * 3.5f;
+        player.SetActive(true);
+
+        Player playerLogic = player.GetComponent<Player>();
+        playerLogic.isHit = false;
+    }
+
+    public void GameOver()
+    {
+        gameOverSet.SetActive(true);
+    }
+
+    public void GameRetry()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
