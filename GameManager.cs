6c7381feeacabf3e9e02140ac2457f0d438f32ad
JangHyunLEe <killiacaridos@gmail.com>
2020-07-26 12:39:58 +0900

    Alpha ver.

--- GameManager.cs
+++ GameManager.cs
@@ -7,13 +7,14 @@ using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.IO;
 
 public class GameManager : MonoBehaviour
 {
     public string[] enemyObjs;
     public Transform[] spawnPoints;
 
-    public float maxSpawnDelay;
+    public float nextSpawnDelay;
     public float curSpawnDelay;
 
     public GameObject player;
@@ -23,18 +24,58 @@ public class GameManager : MonoBehaviour
     public GameObject gameOverSet;
     public ObjectManager objectManager;
 
-    private void Awake()
+    public List<Spawn> spawnList;
+    public int spawnIndex;
+    public bool spawnEnd;
+
+    void Awake()
     {
-        enemyObjs = new string[] { "EnemyS", "EnemyM", "EnemyL" };
+        spawnList = new List<Spawn>();
+        enemyObjs = new string[] { "EnemyS", "EnemyM", "EnemyL", "EnemyB" };
+        ReadSpawnFile();
+    }
+
+    void ReadSpawnFile()
+    {
+        //#1.변수 초기화
+        spawnList.Clear();
+        spawnIndex = 0;
+        spawnEnd = false;
+
+        //#2.리스폰 파일 읽기
+        TextAsset textFile = Resources.Load("Stage 0") as TextAsset;
+        StringReader stringReader = new StringReader(textFile.text);
+
+        //#3.한 줄씩 데이터 저장
+        while (stringReader != null)
+        {
+            string line = stringReader.ReadLine();
+            Debug.Log(line);
+
+            if (line == null)
+                break;
+
+            //#.리스폰 데이터 생성
+            Spawn spawnData = new Spawn();
+            spawnData.delay = float.Parse(line.Split(',')[0]);
+            spawnData.type = line.Split(',')[1];
+            spawnData.point = int.Parse(line.Split(',')[2]);
+            spawnList.Add(spawnData);
+        }
+
+        //#.텍스트 파일 닫기
+        stringReader.Close();
+
+        //#.처서번째 스폰 딜레이 적용
+        nextSpawnDelay = spawnList[0].delay;
     }
 
     void Update()
     {
         curSpawnDelay += Time.deltaTime;
-        if (curSpawnDelay > maxSpawnDelay)
+        if (curSpawnDelay > nextSpawnDelay && !spawnEnd)
         {
             SpawnEnemy();
-            maxSpawnDelay = UnityEngine.Random.Range(0.3f, 2.7f);
             curSpawnDelay = 0;
         }
 
@@ -45,27 +86,54 @@ public class GameManager : MonoBehaviour
 
     void SpawnEnemy()
     {
-        int ranEnemy = UnityEngine.Random.Range(0, 3);
-        int ranPoint = UnityEngine.Random.Range(0, 9);
-        GameObject enemy = objectManager.MakeObj(enemyObjs[ranEnemy]);
-        enemy.transform.position = spawnPoints[ranPoint].position;
+        int enemyIndex = 0;
+        switch (spawnList[spawnIndex].type)
+        {
+            case "S":
+                enemyIndex = 0;
+                break;
+            case "M":
+                enemyIndex = 1;
+                break;
+            case "L":
+                enemyIndex = 2;
+                break;
+            case "B":
+                enemyIndex = 3;
+                break;
+        }
+
+        int enemyPoint = spawnList[spawnIndex].point;
+        GameObject enemy = objectManager.MakeObj(enemyObjs[enemyIndex]);
+        enemy.transform.position = spawnPoints[enemyPoint].position;
 
         Rigidbody2D rigid = enemy.GetComponent<Rigidbody2D>();
         Enemy enemyLogic = enemy.GetComponent<Enemy>();
         enemyLogic.player = player;
         enemyLogic.objectManager = objectManager;
 
-        if (ranPoint == 5 || ranPoint == 6) {  //#.Right Spawn
+        if (enemyPoint == 5 || enemyPoint == 6) {  //#.Right Spawn
             enemy.transform.Rotate(Vector3.back * 90);
             rigid.velocity = new Vector2(enemyLogic.speed*(-1), -1);
         }
-        else if (ranPoint == 7 || ranPoint == 8) {  //#.Left Spawn
+        else if (enemyPoint == 7 || enemyPoint == 8) {  //#.Left Spawn
             enemy.transform.Rotate(Vector3.forward * 90);
             rigid.velocity = new Vector2(enemyLogic.speed, -1);
         }
         else {  //#.Front Spawn
             rigid.velocity = new Vector2(0, enemyLogic.speed*(-1));
         }
+
+        //#.리스폰 인덱스 증가
+        spawnIndex++;
+        if(spawnIndex == spawnList.Count)
+        {
+            spawnEnd = true;
+            return;
+        }
+        //#.다음 리스폰 딜레이 갱신
+        nextSpawnDelay = spawnList[spawnIndex].delay;
+
     }
     public void UpdateLifeIcon(int life)
     {
